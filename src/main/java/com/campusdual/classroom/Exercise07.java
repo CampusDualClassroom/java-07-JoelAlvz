069175d1ebd2099f8b32e3280578b95ba97d8685
Joel Álvarez Rodriguez <joel@joel>
2024-09-26 12:56:52 +0200

    Ejercicio 7 resuelto

--- src/main/java/com/campusdual/classroom/Exercise07.java
+++ src/main/java/com/campusdual/classroom/Exercise07.java
@@ -1,10 +1,14 @@
 package com.campusdual.classroom;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Exercise07 {
 
     public static void main(String[] args) {
-
+        positionInAList(4);
     }
 
     //TODO ↓
@@ -14,18 +18,36 @@ public class Exercise07 {
     // Si lo encuentra, que imprima el siguiente mensaje → El elemento X se encuentra en la posición: Y
     // Si NO lo encuentra, que imprima el siguiente mensaje → El elemento X no se encuentra en la lista.
     public static void positionInAList(int num) {
+        List<Integer> listaNum = Arrays.asList(10,9,8,7,6,5,4,3,2,1);
 
+        int pos = listaNum.indexOf(num - 1) ;
+
+        if(pos != -1){
+            System.out.println("El elemento "+ num +
+                    " se encuentra en la posición: "+ pos);
+        }else{
+            System.out.println("El elemento "+num+
+                    " no se encuentra en la lista.");
+        }
     }
 
     //TODO ↓
     // Que imprima como mensaje la suma de los primeros N números positivos
     public static void sumFirstNaturalNumbers(int num) {
-
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            sum = i + sum;
+        }
+        System.out.println(sum);
     }
 
     //TODO ↓
     // Que imprima por pantalla los N primeros números positivos
     public static void showFirstNaturalNumbers(int num) {
+
+        for (int i = 1; i <= num; i++) {
+            System.out.println(i);
+        }
 
     }
 
